73d527901737f9b8be56658ca76f31a424f43228
bcapuano <bcapuano@asu.edu>
2023-04-22 10:51:23 -0400

    Changed fuzzer to be a bit more efficient considering limitations of library

--- mayhem/fuzz_lib.cpp
+++ mayhem/fuzz_lib.cpp
@@ -2,14 +2,34 @@
 #include "msgpack11.hpp"
 
 
-extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
-    FuzzedDataProvider fdp{data, size};
-
-    std::string orig_string = fdp.ConsumeRemainingBytesAsString();
-    std::string err;
-    auto objs = msgpack11::MsgPack::parse_multi(orig_string, err);
-    for (const auto& obj : objs) {
-        obj.dump();
+msgpack11::MsgPack::array make_array(FuzzedDataProvider& fdp) {
+    msgpack11::MsgPack::array result;
+    std::size_t sz = fdp.ConsumeIntegralInRange(0, 100);
+    result.reserve(sz);
+    for (std::size_t i = 0; i < sz; ++i) {
+        result.emplace_back(fdp.ConsumeIntegral<int>());
     }
+    return result;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
+    FuzzedDataProvider fdp(data, size);
+
+    std::vector<std::pair<std::string, msgpack11::MsgPack>> vec;
+    std::size_t sz = fdp.ConsumeIntegralInRange(0, 100);
+    vec.reserve(sz);
+    for (std::size_t i = 0; i < sz; i++) {
+        vec.emplace_back(fdp.ConsumeRandomLengthString(), make_array(fdp));
+    }
+    msgpack11::MsgPack obj = msgpack11::MsgPack::object{vec.begin(), vec.end()};
+
+    std::string dumped_obj = obj.dump();
+    std::string err;
+    auto parsed = msgpack11::MsgPack::parse(dumped_obj, err);
+
+    if (parsed != obj) {
+        abort();
+    }
+
     return 0;
 }
